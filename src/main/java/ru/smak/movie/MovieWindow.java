f961151ca4a8a3a7e5c9275e55def78664558dd4
Камила <kam20021710@gmail.com>
2022-12-07 22:04:46 +0300

    Обработка событий при нажатии на кнпоки

--- src/main/java/ru/smak/movie/MovieWindow.java
+++ src/main/java/ru/smak/movie/MovieWindow.java
@@ -4,6 +4,10 @@ import ru.smak.gui.GraphicsPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
 
 public class MovieWindow extends JFrame {
     private final GraphicsPanel moviePanel = new GraphicsPanel();
@@ -12,6 +16,7 @@ public class MovieWindow extends JFrame {
     private JButton SelectFile, OK, Play;
     private JSpinner FPS, Duration;
     private JLabel FPSlbl, Durationlbl;
+    private ArrayList<File> files;
     public MovieWindow(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setMinimumSize(minSz);
@@ -28,5 +33,29 @@ public class MovieWindow extends JFrame {
         OK = new JButton("OK");
         Play = new JButton("Play");
 
+        files = new ArrayList<>();
+
+        SelectFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //открывается диалоговое окно с выбором файла
+                var a = new JFileChooser();
+                files.add(a.getSelectedFile());
+            }
+        });
+
+        OK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //видео начинает создаваться
+            }
+        });
+
+        Play.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //видео запускается (кнопка доступна только после того, как видео создано)
+            }
+        });
     }
 }

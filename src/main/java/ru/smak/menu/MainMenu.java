4d55c8b77554e27ec4c15f6d3e50b05f0ddf917b
Анна <ansh00@mail.ru>
2022-12-11 17:16:02 +0300

    Добавлена информация о программе Еще иконки

--- src/main/java/ru/smak/menu/MainMenu.java
+++ src/main/java/ru/smak/menu/MainMenu.java
@@ -1,10 +1,8 @@
 package ru.smak.menu;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
+import java.net.URL;
 
 public class MainMenu extends JFrame {
     private JMenuBar menuBar;
@@ -24,6 +22,10 @@ public class MainMenu extends JFrame {
         file.add(save);
         file.add(saveAs);
         file.add(open);
+        //save.setIcon(new ImageIcon(getClass().getResource("/icons/save.png")));
+        save.setIcon(new ImageIcon("icons/save.png"));
+        saveAs.setIcon(new ImageIcon("icons/saveAs.png"));
+        open.setIcon(new ImageIcon("icons/open.png"));
         save.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -49,6 +51,8 @@ public class MainMenu extends JFrame {
         JMenu edit = new JMenu("Правка");
         JMenuItem cancel = new JMenuItem("Отмена");
         edit.add(cancel);
+        edit.setIcon(createIcon("icons/edit.png"));
+        cancel.setIcon(createIcon("icons/cancel.png"));
         //отмена операции
         cancel.addMouseListener(new MouseAdapter() {
             @Override
@@ -61,12 +65,35 @@ public class MainMenu extends JFrame {
 
     public JMenu createHelpMenu() {
         JMenu help = new JMenu("О программе");
-        help.addActionListener(new ActionListener() {
+        help.setIcon(createIcon("icons/about.png"));
+        help.addMouseListener(new MouseAdapter() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                //вывод текста о программе
+            public void mouseClicked(MouseEvent e) {
+                String about =
+                        "Над программой работали:\n"
+                                + "КФУ ИМиМ группа 05-003\n"
+                                    + "Антонов Максим - 5-6 задание\n"
+                                    + "Депресов Артём - 7 задание\n"
+                                    + "Иванова Инна - 1 задание\n"
+                                    + "Игнатьев Вадим - 8 задание\n"
+                                    + "Кушаева Алиса - 10 задание\n"
+                                    + "Кушаева Камила - 10 задание\n"
+                                    + "Марданова Альбина - 2 задание\n"
+                                    + "Сошников Никита - 3 задание\n"
+                                    + "Филиппов Артур - 4 задание\n"
+                                    + "Шумихина Анна - 9 задание";
+                JOptionPane.showMessageDialog(null, about);
             }
         });
         return help;
+    }
+    protected static ImageIcon createIcon(String path) {
+        URL imgURL = MainMenu.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("File not found " + path);
+            return null;
+        }
     }
 }
